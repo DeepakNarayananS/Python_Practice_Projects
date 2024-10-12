34b359375c428629016840399f9570d60fc77a31
DeepakNarayananS <sdcyberforce4392@gmail.com>
2024-10-12 14:19:35 +0530

    This is the First Project

---
+++ CharacterCount.py
@@ -0,0 +1,9 @@
+# Simple Python code snippet that counts the number of characters in a given sentence
+def count_characters(sentence):
+    return len(sentence)
+
+# Usage
+user_input = input("Enter a sentence: ")
+character_count = count_characters(user_input)
+print(f"The number of characters in the sentence is: {character_count}")
+
